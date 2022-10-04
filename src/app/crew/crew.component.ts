9c096cac49c736a65622bad432c69c35ce02107e
zkrem56 <zkremer702@gmail.com>
2022-10-04 17:32:01 -0500

    Finished part 5 of Studio

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -10,7 +10,9 @@ export class CrewComponent implements OnInit {
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
+    {name: "Ellen Ochoa", firstMission: true},
+    {name: "Raja Chari", firstMission: false},
+    {name: "Jasmin Moghbeli", firstMission: false}
   ];
 
   constructor() { }
@@ -20,6 +22,11 @@ export class CrewComponent implements OnInit {
 
   add(memberName: string, isFirst: boolean){
     this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object){
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
   }
 
 }
