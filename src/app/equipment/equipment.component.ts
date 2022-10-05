30106c804334f617529c65bcfd7c028265ae992d
zkrem56 <zkremer702@gmail.com>
2022-10-05 15:45:25 -0500

    Made both the equipment and experiments list into an array put onto the webpage

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-
+  equipmentBeingEdited: object = null;
+  equipmentList: object[] = [
+    {name: "Habitat dome"},
+    {name: "Drones"},
+    {name: "Food containers"},
+    {name: "Oxygen tanks"}
+  ]
   constructor() { }
 
   ngOnInit() {
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
-
+  experimentsList: object[] = [
+    {name: 'Mars soil sample'},
+    {name: 'Plant growth in habitat'},
+    {name: 'Human bone density'}
+  ];
   constructor() { }
 
   ngOnInit() {
